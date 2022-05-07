af1201576d0c5f6517d9566866cd79daa493e74c
brilliant-potato <darkashoka@yahoo.de>
2022-05-07 16:36:42 +0200

    add difference function

--- backend/PlaceableObject.py
+++ backend/PlaceableObject.py
@@ -1,3 +1,4 @@
+import numpy as np
 class PlaceableObject:
     def __init__(self, long, lat, parameters, ids, ranges, decays, effects):
         # longitude = x, latitude = y -> positions
@@ -12,3 +13,12 @@ class PlaceableObject:
     def get_influence(self, parameter, sensor):
         """Returns float with influence on given coordinate, depending on distance and respective parameter."""
         # TODO: Olis function here
+
+        def difference(sensor_pos, tree_pos, rang, effect, decay):
+            dist = np.linalg.norm(np.array(sensor_pos) - np.array(tree_pos))
+            return min(effect, ((decay * rang / (dist + 0.0000000001)) * effect)), dist
+
+
+        sensor_pos = []
+
+
